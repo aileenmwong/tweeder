382dc4c54a705489497e3de62cb669e83da2efd8
mao hung <maohung71@gmail.com>
2017-09-15 16:20:28 -0400

    try again

---
+++ tweedr/client/src/components/TweedList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Tweed from './Tweed';
+
+class TweedList extends Component {
+
+  componentWillReceiveProps(nextProps) {
+    console.log('Tweed will receive props');
+  }
+
+  componentWillMount() {
+    console.log('Tweed will mount');
+    /* stop from */
+    return false;
+  }
+
+  /* is triggered right after the state change */
+  componentWillUpdate() {
+    console.log('Tweed will update');
+  }
+  /*
+  triggered right after rendering
+  use case: posting to database after the form submisison
+  */
+  componentDidUpdate() {
+    console.log('Tweed did update');
+  }
+
+  render() {
+    console.log('Tweed rendering', this.state);
+    return (
+      <div>
+        <button onClick={this.props.handleSubmit}>
+          </button>
+        { this.props.data.map(tweed => {
+          return <Tweed tweed={tweed} key={tweed.id}/>
+        })}
+      </div>
+    )
+  }
+}
+
+export default TweedList;
